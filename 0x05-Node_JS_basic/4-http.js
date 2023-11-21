74f343d4e89b44b74275e1649e133650a49e0931
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-21 05:39:55 +0100

    fix: semistandard

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,15 @@
-const http = require("http");
+const http = require('http');
 
 const PORT = 1245;
-const HOST = "localhost";
+const HOST = 'localhost';
 const app = http.createServer();
 
-app.on("request", (_, res) => {
-  const body = "Hello Holberton School!";
+app.on('request', (_, res) => {
+  const body = 'Hello Holberton School!';
 
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Content-Length", Buffer.byteLength(body));
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', Buffer.byteLength(body));
   res.end(body);
 });
 
