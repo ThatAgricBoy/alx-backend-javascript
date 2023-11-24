5c27b40116f7fa412d336b0ce6995119a29f519a
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-24 17:53:58 +0100

    unittest

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,19 +1,17 @@
-function calculateNumber(type, a, b) {
-    const rounded_a = Math.round(a);
-    const rounded_b = Math.round(b);
-    
-    if (type === 'SUM') {
-      return rounded_a + rounded_b;
-    }
-    if (type === 'SUBTRACT') {
-      return rounded_a - rounded_b;
-    }
-    if (type === 'DIVIDE') {
-      if (rounded_b === 0) {
-        return 'Error';
-      }
-      return rounded_a / rounded_b;
-    }
-  }
-  
-  module.exports = calculateNumber;
+const calculateNumber = (type, a, b) => {
+  switch (type) {
+    case 'SUM':
+      return Math.round(a) + Math.round(b);
+      break;
+    case 'SUBTRACT':
+      return Math.round(a) - Math.round(b);
+      break;
+    case 'DIVIDE':
+      if (Math.round(b) === 0) return 'Error';
+      return Math.round(a) / Math.round(b);
+      break;
+    default:
+      break;
+  };
+}
+module.exports = calculateNumber;
